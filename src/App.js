62c2f1deaaa3afc35f5edafa0babb69d35ff30d6
beingsamvis@gmail.com <beingsamvis@gmail.com>
2023-07-31 01:30:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { FaArrowDown } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <div className='leftnav'>
+          <ul className='leftul'>
+            <li><a href="#"><img src={require('./images/attlasian.png')} atl='atlassian' height="25px" width="200px"></img></a></li>
+            <li><a className='liconlinks' href="#">Products<p className='arrow'><FaArrowDown/></p></a></li>
+            <li><a className='liconlinks' href="#">Solutions<p className='arrow'><FaArrowDown/></p></a></li>
+            <li><a className='liconlinks' href="#">Resources<p className='arrow'><FaArrowDown/></p></a></li>
+          </ul>
+        </div>
+        <div className='rightnav'>
+        <ul className='rightul'>
+            <li><a className='riconlinks' href="#"><img src={require('./images/search.png')} atl='search' height="20px" width="20px"></img></a></li>
+            <li><a className='rsiconlinks' href="#">Try now</a></li>
+            <li><a className='riconlinks' href="#">Sign in </a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className='mainpage'>
+        <img src={require('./images/bodyimage.jpg')} className='mainimage' alt='backgroud'></img>
+        <div className='info'>
+          <h1>Move work<br/>forward</h1>
+          <p>Atlassian helps teams work smarter and faster,<br/>together</p>
+          <button className='learnmore'>Learn More</button>
+        </div>
+      </div>
+
     </div>
   );
 }
